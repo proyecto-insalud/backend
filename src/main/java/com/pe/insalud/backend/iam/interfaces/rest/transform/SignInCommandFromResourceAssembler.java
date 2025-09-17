5d9e2daa5f8ad3d8ff2ea194655e788b1837f63a
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-09-17 15:07:48 -0500

    feat(transform): convert SignInResource to SignInCommand

---
+++ src/main/java/com/pe/insalud/backend/iam/interfaces/rest/transform/SignInCommandFromResourceAssembler.java
@@ -0,0 +1,12 @@
+package com.pe.insalud.backend.iam.interfaces.rest.transform;
+
+import com.pe.insalud.backend.iam.domain.model.commands.SignInCommand;
+import com.pe.insalud.backend.iam.interfaces.rest.resources.SignInResource;
+
+public class SignInCommandFromResourceAssembler {
+
+
+    public static SignInCommand toCommandFromResource(SignInResource signInResource) {
+        return new SignInCommand(signInResource.username(), signInResource.password());
+    }
+}
